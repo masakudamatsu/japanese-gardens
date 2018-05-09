3cf69e5bc1c388d02136add5dbe3416ab1e7149c
Unknown <masakudamatsu@gmail.com>
2018-05-10 08:38:07 +0900

    Add carousel #2

---
+++ resources/js/main.js
@@ -0,0 +1,25 @@
+var object = {};
+// const = ;
+
+// Carousel
+$(document).ready(function() {
+  $('.carousel').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true
+  });
+});
+
+
+// jQuery
+$(document).ready(function() {
+  // Here write event handers
+});
+
+// Define functions
+object.Function1 = function() {
+};
+
+object.Function2 = function() {
+};
