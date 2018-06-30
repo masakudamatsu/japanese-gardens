bcc17c232a0e37ae795d19f28b9b93b99870af88
Unknown <masakudamatsu@gmail.com>
2018-06-30 20:05:40 +0900

    Fix the carousel with ScrollMagic #8
    Comment out the parallax code

--- resources/js/main.js
+++ resources/js/main.js
@@ -14,23 +14,30 @@ $(document).ready(function() {
 
   // ScrollMagic
   var controller = new ScrollMagic.Controller();
-  
+  // Pin the carousel
+  var pinCarousel = new ScrollMagic.Scene({
+    triggerElement: '.parallax',
+    triggerHook: 0
+  })
+  .setPin('.parallax', {pushFollowers: false})
+  .addIndicators()    // For debugging
+  .addTo(controller);
 
-  // Parallax
-  var parallaxElements = $('.parallax'),
-      parallaxQuantity = parallaxElements.length;
-  $(window).on('scroll', function() {
-    window.requestAnimationFrame(function() {
-      for (var i = 0; i < parallaxQuantity; i++) {
-        var currentElement = parallaxElements.eq(i);
-        // Record how much has been scrolled (the number of pixels that are hidden from view above)
-        var scrolled = $(window).scrollTop();
-        currentElement.css({
-          'transform': 'translate3d(0, '+scrolled * 1 + 'px, 0)'
-        });
-      }
-    });
-  });
+  // // Parallax
+  // var parallaxElements = $('.parallax'),
+  //     parallaxQuantity = parallaxElements.length;
+  // $(window).on('scroll', function() {
+  //   window.requestAnimationFrame(function() {
+  //     for (var i = 0; i < parallaxQuantity; i++) {
+  //       var currentElement = parallaxElements.eq(i);
+  //       // Record how much has been scrolled (the number of pixels that are hidden from view above)
+  //       var scrolled = $(window).scrollTop();
+  //       currentElement.css({
+  //         'transform': 'translate3d(0, '+scrolled * 1 + 'px, 0)'
+  //       });
+  //     }
+  //   });
+  // });
 
 }); // End of jQuery
 
